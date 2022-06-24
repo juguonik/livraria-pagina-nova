2b593afda67d8bd042a7dce8139dbe79c6c43ec8
unknown <juliana_guonik@hotmail.com>
2022-06-23 21:09:02 -0300

    Adding images

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
-const db = require('./database')
+const db = require('./database');
+const upload = require('./config/upload')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(upload.any())
+app.use(express.static('src/public/images'))
 
 db.hasConnection()
 
